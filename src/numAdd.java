0432339caf876845681c5523c1d187f75f4ecf4e
krabhi79 <krabhishek399@outlook.com>
2022-08-28 18:19:17 +0530

    first commit

---
+++ src/numAdd.java
@@ -0,0 +1,10 @@
+public class numAdd {
+    public static void main(String[] args) {
+        System.out.println ("The sum of these numbers is");
+        int a = 12;
+        int b = 99;
+        int c = 4;
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+}

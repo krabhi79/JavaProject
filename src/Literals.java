d7a70111d1a722d2b18cb4dd54e1207fd09cf154
krabhi79 <krabhishek399@outlook.com>
2022-08-29 14:12:44 +0530

    Added Literals

---
+++ src/Literals.java
@@ -0,0 +1,11 @@
+public class Literals {
+    public static void main(String[] args) {
+        int age = 39;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double D1 = 4.67d;
+        byte height = 41;
+        long length = 466666666666666l;
+        System.out.println(length);
+    }
+}

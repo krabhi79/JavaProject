c7bcf8d3fcd0a0f7adae48f06ccffe362923023e
krabhi79 <krabhishek399@outlook.com>
2022-11-05 21:16:53 +0530

    qoutientRemainder

---
+++ src/qoutientRemainder.java
@@ -0,0 +1,10 @@
+public class qoutientRemainder {
+    public static void main(String[] args){
+        int a = 48, b =8;
+        int quotient = a / b;
+        int remainder = a % b;
+        System.out.println("quotient is " + quotient);
+        System.out.println("remainder is " + remainder);
+    }
+
+}

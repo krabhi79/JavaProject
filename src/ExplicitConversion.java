5c9d9be0804fc360258ce1f7fa963730b034ac89
krabhi79 <krabhishek399@outlook.com>
2022-11-06 20:45:58 +0530

    ExplicitConversion

---
+++ src/ExplicitConversion.java
@@ -0,0 +1,8 @@
+public class ExplicitConversion {
+        public static void main(String[] args) {
+            float x, y;
+            x = 26 / 3;
+            y = (float) 26 / 3;
+            System.out.println("x = " + x + " y = " + y);
+    }
+}
